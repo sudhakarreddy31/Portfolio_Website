f1f646c2bdd99501ac8691ecaee7ee0923f1270d
sudhakarreddy <sudhakarreddymoola31@gmail.com>
2023-11-01 20:04:08 +0530

    Deploy in Server

--- website/website/settings.py
+++ website/website/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,9 +130,13 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),  # Replace 'static' with your actual static files directory
-# ]
+STATIC_ROOT = (os.path.join(BASE_DIR,'staticfiles'))
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'), 
+]
+
+
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
