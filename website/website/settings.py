8431ef2b04a17d5bfbe42e80aed1186bcd5b979e
sudhakarreddy <sudhakarreddymoola31@gmail.com>
2023-11-02 17:42:06 +0530

    updated settings files

--- website/website/settings.py
+++ website/website/settings.py
@@ -131,9 +131,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = (os.path.join(BASE_DIR,'staticfiles'))
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'), 
-]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
 django_heroku.settings(locals())
@@ -146,3 +144,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+heroku config:set DISABLE_COLLECTSTATIC=1
