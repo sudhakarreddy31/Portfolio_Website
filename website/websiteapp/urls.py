cfb97d06cdf9dc1a6ea3f8fe46ed55a6d2724e84
sudhakarreddy <sudhakarreddymoola31@gmail.com>
2023-11-02 17:20:02 +0530

    Updated urls files

--- website/websiteapp/urls.py
+++ website/websiteapp/urls.py
@@ -3,10 +3,10 @@ from . import views
 from .views import AboutView, ContactView, GalleryListView, HomeView, SkillsView, WorkView
 
 urlpatterns = [
-    path('home/', HomeView.as_view(), name='home'),
-    path('about/', AboutView.as_view(), name='About'),
-    path('skills/', SkillsView.as_view(), name='Skills'),
-    path('work/', WorkView.as_view(), name='Work'),
-    path('contact/', ContactView.as_view(), name='Contact'),
-    path('gallery/', GalleryListView.as_view(), name='Gallery'),
+    path('', HomeView.as_view(), name='home'),
+    path('about', AboutView.as_view(), name='About'),
+    path('skills', SkillsView.as_view(), name='Skills'),
+    path('work', WorkView.as_view(), name='Work'),
+    path('contact', ContactView.as_view(), name='Contact'),
+    path('gallery', GalleryListView.as_view(), name='Gallery'),
 ]
